8936f207896124f61910d8b1f8aa478067c35fe7
james01123 <erichemming11@gmail.com>
2022-09-11 17:04:46 -0700

    Fixed indentation issues.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+/**
+ * Runs a customized 'hello world!' type message.
+ *
+ * @author Eric H
+ * @version 1
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Football is back!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Football is back!");
+  }
 }
